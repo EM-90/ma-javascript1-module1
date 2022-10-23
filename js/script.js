a16401ddc749f41753661af79ab7b631119349d0
Eirik Michielsen <eirik.michielsen@gmail.com>
2022-10-23 12:26:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,15 +36,45 @@ heading.style.fontSize = "2em";
 heading.className = "subheading";
 
 //Question 5
-
 let paragraphs = document.querySelectorAll("p");
 
-for (let i = 0; i <= paragraphs.length; i++) {
+for (let i = 0; i < paragraphs.length; i++) {
   paragraphs[i].style.color = "red";
 }
-
 //Question 6 not finished
 
-const resultsContainer = document.querySelector(".results");
+let resultsContainer = document.querySelector(".results");
 resultsContainer.innerHTML = "<p>New Content</p>";
-resultsContainer.style.background.color = "yellow";
+resultsContainer.style.backgroundColor = "yellow";
+
+//Question 7
+
+function showNames(list) {
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i].name);
+  }
+}
+
+showNames(cats);
+
+//Question 8
+
+function createCats(cats) {
+  let list = "";
+
+  for (let i = 0; i < cats.length; i++) {
+    list += `<h5>${cats[i].name}</h5><p>${cats[i].age}</p>`;
+
+    console.log(list);
+  }
+  const htmlFinal = "<div>" + list + "</div>";
+  return htmlFinal;
+}
+
+const container = document.querySelector(".cat-container");
+
+const newHtml = createCats(cats);
+
+container.innerHTML = newHtml;
+
+createCats(cats);
